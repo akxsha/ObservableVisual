97e1c7d827e01063328b931947f1e22960950211
akxsha <43390570+akxsha@users.noreply.github.com>
2022-01-02 12:56:34 -0800

    Update and rename notebook.html to notebook.js

---
+++ notebook.js
@@ -0,0 +1,8 @@
+<div id="observablehq-c9d70368"></div>
+<p>Credit: <a href="https://observablehq.com/@akxsha/electric-consumption-and-co2-emissions">Electric Consumption and CO2 Emissions by akxsha</a></p>
+
+<script type="module">
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import define from "https://api.observablehq.com/@akxsha/electric-consumption-and-co2-emissions.js?v=3";
+new Runtime().module(define, Inspector.into("#observablehq-c9d70368"));
+</script>
